2c10455e5b0dd232da856d3b96e3e46a066cbf56
sneha-gowda <snehagowda9899@gmail.com>
2022-01-03 23:51:45 +0530

    created schema an model and insert operation

--- assignments/assignment_3/app.js
+++ assignments/assignment_3/app.js
@@ -1,11 +1,52 @@
 const express=require('express');
 const app = express();
-app.set('view engine', 'ejs')
-let users=[{name: 'John', email: 'john@gmail.com'},{name: 'Rony', email: 'tony@gmail.com'},{name:"Toy",email:"roy@gmail.com"}];
 
-// Middlewares 
-// 1. to parse incoming  application / x - www - form - urlencoded content type  to string
-// describes form data that is sent in a single block in the HTTP message body.
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/assignment_4",{}).then(()=>{
+    console.log("DB connected");
+}).catch(err => console.log(err));
+
+const userSchema=new mongoose.Schema({
+    name:{
+        type:"string",
+        required: true
+    },
+    email:{
+        type:"string",
+        required: true,
+        unique: true,
+    },
+    isPromoted:{
+        type:Boolean,
+        default:false
+    }
+})
+const userCollection=new mongoose.model("User",userSchema);
+
+const insertDocToUser=async (doc)=>{
+    try{
+        const Docs=new userCollection(doc);
+        const res=await Docs.save()
+        console.log(res)
+    }catch(err){
+        console.log(err);
+    };
+}
+// insertDocToUser({name:"Sneha",email:"sneha@gmail.com"})
+
+const getDocsOfUser=async ()=>{
+    try {
+        console.log("ji");
+        const Docs = await userCollection.find();
+        console.log(Docs,"here");
+        return Docs
+    } catch (err) {
+        console.log(err);
+    };
+}
+
+app.set('view engine', 'ejs')
+
 app.use(express.urlencoded({
     extended: true
 })) 
@@ -13,7 +54,9 @@ app.use(express.urlencoded({
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req,res) => {
-    res.render('homepage.ejs',{data:users});
+    getDocsOfUser().then((data)=>{
+        res.render('homepage.ejs', { data: data });
+    })
 })
 app.get('/form', (req, res) => {
     res.render('form.ejs');
@@ -24,9 +67,14 @@ app.post('/user/add',(req,res)=>{
         name: req.body.name,
         email: req.body.email
     }
-    users.push(obj)
-    res.render('homepage.ejs', { data: users });
+    insertDocToUser(obj)
+    getDocsOfUser().then((data) => {
+        res.render('homepage.ejs', { data: data });
+    })
 } );
+app.put("/user",(req,res)=>{
+    console.log(req,"hehe");
+})
 app.listen(3000,()=>{
     console.log("im listening");
 })
