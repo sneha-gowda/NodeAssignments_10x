63149f72eec82cde15f38d2bfb59f86bd6ae74b6
sneha-gowda <snehagowda9899@gmail.com>
2022-01-04 02:13:32 +0530

    Assignment completed

--- assignments/assignment_3/app.js
+++ assignments/assignment_3/app.js
@@ -1,11 +1,30 @@
 const express=require('express');
+
+// Connecting port
 const app = express();
 
+// Seting up the environment 
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({
+    extended: true
+})) 
+app.listen(3000, () => {
+    console.log("Port 3000 is listening");
+})
+app.use(express.static(__dirname + '/public'));
+
+// to use put and delete method over rides
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'))
+
+// Connecting Data Base
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/assignment_4",{}).then(()=>{
     console.log("DB connected");
 }).catch(err => console.log(err));
 
+// Making Schema for User
 const userSchema=new mongoose.Schema({
     name:{
         type:"string",
@@ -17,64 +36,108 @@ const userSchema=new mongoose.Schema({
         unique: true,
     },
     isPromoted:{
-        type:Boolean,
-        default:false
+        type:String,
+        default:"Null"
     }
 })
+
+// Creating model for User Collection
 const userCollection=new mongoose.model("User",userSchema);
 
-const insertDocToUser=async (doc)=>{
-    try{
-        const Docs=new userCollection(doc);
-        const res=await Docs.save()
-        console.log(res)
-    }catch(err){
-        console.log(err);
-    };
-}
-// insertDocToUser({name:"Sneha",email:"sneha@gmail.com"})
+// -------------------CRUD OPERATION STARTS-------------------------------------------
 
-const getDocsOfUser=async ()=>{
+// READ: Reading documents of User Collection
+const getDocsOfUser = async () => {
     try {
-        console.log("ji");
         const Docs = await userCollection.find();
-        console.log(Docs,"here");
         return Docs
     } catch (err) {
         console.log(err);
     };
 }
 
-app.set('view engine', 'ejs')
-
-app.use(express.urlencoded({
-    extended: true
-})) 
-// 2. to link public file 
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', (req,res) => {
-    getDocsOfUser().then((data)=>{
+// Route Home 
+app.get('/', (req, res) => {
+    getDocsOfUser().then((data) => {
         res.render('homepage.ejs', { data: data });
     })
 })
+
+// Route Form
 app.get('/form', (req, res) => {
     res.render('form.ejs');
 })
+
+
+// CREAT: Inserting Values to the User collection
+const insertDocToUser = async (doc) => {
+    try {
+        const Docs = new userCollection(doc);
+        const res = await Docs.save()
+
+    } catch (err) {
+        console.log(err);
+    };
+}
+// Handing Post request from Form
 app.post('/user/add',(req,res)=>{
     // console.log(req.body)
     const obj={
         name: req.body.name,
         email: req.body.email
+
     }
     insertDocToUser(obj)
     getDocsOfUser().then((data) => {
-        res.render('homepage.ejs', { data: data });
+        res.redirect("/")
     })
 } );
-app.put("/user",(req,res)=>{
-    console.log(req,"hehe");
-})
-app.listen(3000,()=>{
-    console.log("im listening");
+
+
+
+// UPDATE: Updating the isPromoted field of document
+const updateDocofUser = async (_id) => {
+    try {
+       userCollection.findOne({ _id: _id }, { isPromoted: 1 }).then((data)=>{
+            prevVal=data.isPromoted
+            if(prevVal=="true"){
+                prevVal="false"
+            }
+            else{
+                prevVal="true"
+            }
+            userCollection.findByIdAndUpdate(_id,{isPromoted:prevVal},{new:true}).then(res=>{
+                console.log(res);
+            });
+
+       });
+       
+        // const res = await userCollection.findByIdAndUpdate(id,{isPromoted:})
+    } catch (err) { console.log(err) }
+}
+// Handeling put request 
+
+app.put("/users/:_id",(req,res)=>{
+    updateDocofUser(req.params._id);
+    getDocsOfUser().then((data) => {
+        res.redirect('/');
+    })
+});
+
+//DELETING: Deleting doc User
+const deleteDocofUser= async (_id) =>{
+    userCollection.findByIdAndDelete(_id).then((data) => {
+        console.log(data)
+    }).catch((err) => {
+        console.log(err);
+    })
+};
+
+
+// Handling delete request
+
+app.delete("/users/:_id",(req, res)=>{
+    deleteDocofUser(req.params._id).then((data) => {
+        res.redirect('/');
+    })
 })
